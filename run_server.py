80bb32185b772df1aee9c567674ebcf2eb956a05
ashnikh78 <ashnikh78@gmail.com>
2024-07-31 16:48:35 +0530

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def run_server():
-    command = ["uvicorn", "main:app", "--host", "127.0.0.1", "--port", "8081", "--reload"]
+    command = ["uvicorn", "main:app", "--host", "127.0.0.1", "--port", "8086", "--reload"]
     subprocess.run(command)
 
 if __name__ == "__main__":
